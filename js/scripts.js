382794691375c1e011719e1a89a56ffa10aca73c
Jake Nagel and Reese Lee <reese@>
2019-04-01 16:51:30 -0700

    modify js scripts to include for and while loops

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,27 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var groceries = ["item1", "item2", "item3", "item4", "item5"];
+    var arr = [];
     groceries.forEach(function(grocery){
       var userInput = $("input#" + grocery).val();
+      arr.push(userInput);
       $("." + grocery).text(userInput);
     });
 
     things = [$("#item1").val(), $("#item2").val(), $("#item3").val() ,$("#item4").val() ,$("#item5").val()];
 
-    console.log(things);
-    things.forEach
-      $("#fig0").append("<li>" + $("#item1").val() + "</li>");
-      $("#fig0").append("<li>" + $("#item2").val() + "</li>");
-      $("#fig0").append("<li>" + $("#item3").val() + "</li>");
-      $("#fig0").append("<li>" + $("#item4").val() + "</li>");
-      $("#fig0").append("<li>" + $("#item5").val() + "</li>");
+      var uppers = arr.map(function(thing) {
+        return thing.toUpperCase();
+      });
+      uppers.sort();
+
+      for(var i = 0; i < uppers.length; i += 1){
+      $("#fig0").append("<li>" + uppers[i] + "</li>");
+    };
 
       $("#formOne").hide();
       $(".shopList").show();
+
 
     event.preventDefault();
   });
